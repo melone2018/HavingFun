640fae51fbda8da9d11be71c602df833c4c15c2b
Rui <liurui320916@gmail.com>
2018-07-25 19:52:27 -0500

    Created basic UI for login, sign up and homepage

--- app/src/main/java/com/fun/havingfun/ui/signup/SignupActivity.java
+++ app/src/main/java/com/fun/havingfun/ui/signup/SignupActivity.java
@@ -1,13 +1,15 @@
-package com.fun.havingfun;
+package com.fun.havingfun.ui.signup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.fun.havingfun.R;
+
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
 }
